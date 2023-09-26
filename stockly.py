9780de0071d5e9a5b69f79b36d7015cd79bc463d
Christian Ray <143613191+CARay1502@users.noreply.github.com>
2023-09-25 21:51:21 -0400

    Add files via upload

--- stockly.py
+++ stockly.py
@@ -1,45 +1,52 @@
+import yfinance as yf
 import streamlit as st
 import pandas as pd 
-import numpy as np
-import yfinance as yf
-import plotly.graph_objects as go
 from pandas_datareader import data as pdr
+import time as t 
 
-#--- CONVERT YFINANCE DATA TO PANDAS --- 
-yf.pdr_override() 
+# --- PAGE CONFIG --- 
+st.set_page_config(
+    page_title="Stockly",
+    page_icon="chart_with_upwards_trend",
+    layout="centered",
+)
+yf.pdr_override()
+# --- TICKER INFO & DATA --- 
+st.subheader("Stock Data AI Aggregator! Relevant Data & News...")
 
-#--- STOCK DATA & TICKERS --- 
-#tickers = ("APPL, MSFT, GOOG, NVDA") 
+with st.sidebar:
+    st.header("Welcome to Stockly!")
+    st.subheader("Investing made easy!") 
+    st.text("Stockly is a tech startup focused in making financial data easy and accurate! Make informed decisions with faster and more relevant information!")
+    st.divider()
+    st.caption("Navigate over to our Test Data Engine")
 
-#--- STREAMLIT FRAMEWORK --- 
-st.title("Welcome to stock.ly")
-st.caption("select a stock ticker to analyze:") 
-
-    #--- STOCK SELECTION FORM --- 
-with st.form("stock_selector"):
-    STOCK_TICKER = st.text_input("Enter the Stock Ticker Symbol here...")
-    LENGTH_TIME = st.selectbox(
-        label= "Length of time", 
-        label_visibility="visible",
-        options= ('1m', '1h'),
-    ) 
-    INTERVAL= '5m'
-    submitted = st.form_submit_button("Get Stock Data!") 
-
-if submitted:
-    if not STOCK_TICKER:
+with st.form("Stock Ticker Symbol Selector"):
+    stockTicker = st.text_input("Enter the Stock Ticker Symbol here...") 
+    submitted = st.form_submit_button("Get Data!") 
+if submitted: 
+    if not stockTicker:
         st.info("Please provide a valid Stock Ticker Label") 
         st.stop()
-    elif not LENGTH_TIME:
-        st.info("Please select a valid length of time") 
-        st.stop() 
-    elif not INTERVAL:
-        st.info("Problem with interval") 
-        st.stop() 
 
-    #--- STOCK DATA DISPLAY --- 
-with st.spinner("Collecting your data..."):
+with st.spinner("Collecting your data..."): 
     if submitted:
-        data = pdr.get_data_yahoo(STOCK_TICKER, LENGTH_TIME, INTERVAL)
-        df = pd.DataFrame(data)
-        st.table(df)
+        data = yf.Ticker(stockTicker).info
+        headlines = yf.Ticker(stockTicker).news
+        tab1, tab2 = st.tabs(["Data", "News"]) 
+        with tab1:
+            st.subheader("Finance Data Relevant to Selected Stock:")
+            st.write(data) 
+        with tab2:
+            st.subheader("News Lines Relevant to Selected Stock:") 
+            st.dataframe(
+                headlines,
+                use_container_width=True,
+                hide_index=True,
+                column_config={
+                    "link": st.column_config.LinkColumn(
+                        "Links",
+                    ),
+                }
+            )
+            
